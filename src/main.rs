b54452bf9110c072ee9534b6a15947ac5a4defbe
George Cristian <george.crist5@gmail.com>
2023-01-08 23:09:59 +0100

    Implemente simple read write for the TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use tokio::net::TcpListener;
-
-const BUFFER_SIZE:usize = 1024;
+use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt}};
 
 #[tokio::main]
 async fn main() {
     //Suspend the current task until our future is ready to be returned
-    let listener_result = TcpListener::bind("localhost:8080").await;
+    let listener_result = TcpListener::bind("localhost:8888").await;
 
     let listener = match listener_result {
         Ok(listener) => listener,
@@ -13,7 +11,26 @@ async fn main() {
     };
 
     // Accept any new incoming connections, and get the socket and address
-    let (socket, addr) = listener.accept().await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    
+    // Get split sockets because then we run into moving errors
+    let (read_socket, mut write_socket) = socket.split();
 
-    let mut buffer = [0u8; BUFFER_SIZE];
+    let mut buf_reader = BufReader::new(read_socket);
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = buf_reader.read_line(&mut line).await.unwrap();
+
+        // Check if the client disconnect and if there is no data left
+        if bytes_read == 0 {
+            break;
+        }
+
+        write_socket.write_all(line.as_bytes()).await.unwrap();
+
+        // Clear the contents of the string because otherwise it will just append the next message to the existing string
+        line.clear();
+    }
+    
 }
