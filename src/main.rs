93783888db04865832fab5ffec4acd08a102b970
George Cristian <george.crist5@gmail.com>
2023-01-08 22:42:09 +0100

    Added tcp listener for server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use tokio::net::TcpListener;
+
+const BUFFER_SIZE:usize = 1024;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    //Suspend the current task until our future is ready to be returned
+    let listener_result = TcpListener::bind("localhost:8080").await;
+
+    let listener = match listener_result {
+        Ok(listener) => listener,
+        Err(error) => panic!("Unable to create TCP listener on port 8080: {:?}", error)
+    };
+
+    // Accept any new incoming connections, and get the socket and address
+    let (socket, addr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8; BUFFER_SIZE];
 }
