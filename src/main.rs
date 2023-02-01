9273f8fa8355c73e3267e59d580d3d2b98f5e717
George Cristian <george.crist5@gmail.com>
2023-02-01 22:41:26 +0100

    Added support for multiple clients using tokio tasks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt}};
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}};
 
 #[tokio::main]
 async fn main() {
@@ -10,27 +10,35 @@ async fn main() {
         Err(error) => panic!("Unable to create TCP listener on port 8080: {:?}", error)
     };
 
-    // Accept any new incoming connections, and get the socket and address
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    
-    // Get split sockets because then we run into moving errors
-    let (read_socket, mut write_socket) = socket.split();
-
-    let mut buf_reader = BufReader::new(read_socket);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = buf_reader.read_line(&mut line).await.unwrap();
+        // Accept any new incoming connections, and get the socket and address
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        
+        // Create a new async task for the newly connected client
+        // Note the presence of async move which basically says that the code block is an async
+        // task so you do not need to write it as a separate function
+        tokio::spawn(async move {
+            // Get split sockets because then we run into moving errors
+            let (read_socket, mut write_socket) = socket.split();
 
-        // Check if the client disconnect and if there is no data left
-        if bytes_read == 0 {
-            break;
-        }
+            let mut buf_reader = BufReader::new(read_socket);
+            let mut line = String::new();
 
-        write_socket.write_all(line.as_bytes()).await.unwrap();
+            loop {
+                let bytes_read = buf_reader.read_line(&mut line).await.unwrap();
 
-        // Clear the contents of the string because otherwise it will just append the next message to the existing string
-        line.clear();
+                // Check if the client disconnect and if there is no data left
+                if bytes_read == 0 {
+                    break;
+                }
+
+                write_socket.write_all(line.as_bytes()).await.unwrap();
+
+                // Clear the contents of the string because otherwise it will just append the next message to the existing string
+                line.clear();
+            }
+        });
+
     }
     
 }
